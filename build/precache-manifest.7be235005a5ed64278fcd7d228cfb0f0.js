7d002cb52dbb363a2f9624cc795bf1074fc5eea5
comrade-07 <jmccalustre07@gmail.com>
2023-05-08 00:15:51 +0800

    deploy birthday

---
+++ build/precache-manifest.7be235005a5ed64278fcd7d228cfb0f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0695f796012963489c15f5c1b5fd4e1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7986e97773abffc7b996",
+    "url": "/static/css/main.d9dfb57b.chunk.css"
+  },
+  {
+    "revision": "55895509a15293575658",
+    "url": "/static/js/2.5faffb1e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5faffb1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7986e97773abffc7b996",
+    "url": "/static/js/main.9a3df06f.chunk.js"
+  },
+  {
+    "revision": "3cc7102f3adc2cfb750d",
+    "url": "/static/js/runtime-main.084a06cf.js"
+  },
+  {
+    "revision": "90118e04a5ab527cb88749db824d7b7b",
+    "url": "/static/media/githubLogo.90118e04.svg"
+  }
+]);
